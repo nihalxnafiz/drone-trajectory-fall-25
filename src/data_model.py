57168d98487152a0c17d1db7687e29e4a743d9a9
Ayush Baid <ayushrakeshbaid@gmail.com>
2025-08-29 17:36:04 -0700

    Add boilerplate code, notebook, and unit tests

---
+++ src/data_model.py
@@ -0,0 +1,29 @@
+"""Data models for the camera and user specification."""
+
+
+class DatasetSpec:
+    """
+    Data model for specifications of an image dataset.
+    """
+
+    pass
+
+
+class Camera:
+    """
+    Data model for a simple pinhole camera.
+
+    References:
+    - https://github.com/colmap/colmap/blob/3f75f71310fdec803ab06be84a16cee5032d8e0d/src/colmap/sensor/models.h#L220
+    - https://en.wikipedia.org/wiki/Pinhole_camera_model
+    """
+
+    pass
+
+
+class Waypoint:
+    """
+    Waypoints are positions where the drone should fly to and capture a photo.
+    """
+
+    pass
